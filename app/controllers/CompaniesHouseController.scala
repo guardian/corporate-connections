fb23ff3490b4f7dcc0d54fc7005924333785f30f
Thomas King <102960825+tkgnm@users.noreply.github.com>
2023-02-07 19:12:28 +0000

    attempt at 2

--- app/controllers/CompaniesHouseController.scala
+++ app/controllers/CompaniesHouseController.scala
@@ -1,7 +1,12 @@
 package controllers
 
+import models.OfficerAppointmentResponse
 import play.api.mvc._
 import services.CompaniesHouseService
+import io.circe._
+import io.circe.generic.auto._
+import io.circe.parser._
+import io.circe.syntax._
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -9,4 +14,9 @@ class CompaniesHouseController(components: ControllerComponents, companiesHouseS
   def getAppointmentsProxy(officerId: String) = Action.async {
     companiesHouseService.getAppointments(officerId).map(json => Ok(json))
   }
+  def getAppointments(officerId: String) = Action.async {
+//    make this call a different function inside CompaniesHouseService
+    companiesHouseService.getCompanyNumbers(officerId).map(json => Ok(json))
+  }
+
 }
--- app/services/CompaniesHouseService.scala
+++ app/services/CompaniesHouseService.scala
@@ -1,14 +1,26 @@
 package services
 
+import io.circe._
+import io.circe.generic.auto._
+import io.circe.parser._
+import io.circe.syntax._
 import play.api.libs.ws.{WSAuthScheme, WSClient}
 
 import scala.concurrent.{ExecutionContext, Future}
 
-class CompaniesHouseService(wsClient: WSClient, apiKey: String)(implicit ec: ExecutionContext) {
+import models.OfficerAppointmentResponse
 
+
+class CompaniesHouseService(wsClient: WSClient, apiKey: String)(implicit ec: ExecutionContext) {
   def getAppointments(officerID: String): Future[String] = {
     val url = "https://api.company-information.service.gov.uk/officers/" + officerID + "/appointments"
     fetch(url = url)
+  }
+
+  def getCompanyNumbers(officerID: String): Future[String] = {
+    val url = "https://api.company-information.service.gov.uk/officers/" + officerID + "/appointments"
+    val decoded = decode[OfficerAppointmentResponse](fetch(url = url))
+    decoded.map(resp => resp).toString
   }
   private def fetch(url: String): Future[String] = {
     wsClient
